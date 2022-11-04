d2b21ee6e4b8073d819040c9f0b5370003ee6df2
Saulo Gusmao <saulogusmaoribeiro@hotmail.com>
2022-11-03 22:27:11 -0300

    AuthPage part1

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -9,20 +9,33 @@ class AuthForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Form(
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Nome'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'E-mail'),
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Senha'),
-            ),
-          ],
+      margin: EdgeInsets.all(20),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Nome'),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(labelText: 'Senha'),
+              ),
+              SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Entrar'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text('Criar uma nova conta?'),
+              ),
+            ],
+          ),
         ),
       ),
     );
