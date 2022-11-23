0f1848eae29902ff92d61f8d3b675479c27950df
Saulo Gusmao <saulogusmaoribeiro@hotmail.com>
2022-11-22 21:26:41 -0300

    Picked Image

--- lib/components/user_image_picker.dart
+++ lib/components/user_image_picker.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
   final void Function(File image) onImagePick;
@@ -17,6 +18,23 @@ class UserImagePicker extends StatefulWidget {
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+      widget.onImagePick(_image!);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -24,9 +42,10 @@ class _UserImagePickerState extends State<UserImagePicker> {
         CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
+          backgroundImage: _image != null ? FileImage(_image!) : null,
         ),
         TextButton(
-          onPressed: () {},
+          onPressed: _pickImage,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
