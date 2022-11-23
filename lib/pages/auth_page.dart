de62ca2d1ff3bec570c733883d8d070aec6b6a59
Saulo Gusmao <saulogusmaoribeiro@hotmail.com>
2022-11-22 21:46:25 -0300

    Auth Logic1

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -15,16 +15,25 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
 
-  void _handleSubmit(AuthFormData formData) {
-    setState(() {
-      _isLoading = true;
-    });
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+      if (formData.isLogin) {
+        //login
+      } else {
+        //Signup
+      }
+    } catch (error) {
+      //tratar erro
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
     print('AuthPage...');
     print(formData.email);
-
-    setState(() {
-      _isLoading = false;
-    });
   }
 
   @override
