28a97122ba0deeb78b07b88d33bbb971ca971dfb
Saulo Gusmao <saulogusmaoribeiro@hotmail.com>
2022-11-23 17:50:42 -0300

    Auth or AppPage

---
+++ lib/pages/auth_or_app_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:proj_chat/core/models/chat_user.dart';
+import 'package:proj_chat/core/services/auth/auth_mock_service.dart';
+import 'package:proj_chat/pages/auth_page.dart';
+import 'package:proj_chat/pages/chat_page.dart';
+import 'package:proj_chat/pages/loading_page.dart';
+
+class AuthOrAppPage extends StatelessWidget {
+  const AuthOrAppPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<ChatUser?>(
+        stream: AuthMockService().userChanges,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return LoadingPage();
+          } else {
+            return snapshot.hasData ? ChatPage() : AuthPage();
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Chat page'),
+      ),
+    );
+  }
+}
