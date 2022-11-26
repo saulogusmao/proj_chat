22c5bcb78ac670359a08276b0e661927514f47a4
Saulo Gusmao <saulogusmaoribeiro@hotmail.com>
2022-11-26 19:33:32 -0300

    Construtor Factory

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:proj_chat/core/services/auth/auth_mock_service.dart';
+import 'package:proj_chat/core/services/auth/auth_service.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -17,7 +18,7 @@ class ChatPage extends StatelessWidget {
             Text('Chat page'),
             TextButton(
               onPressed: () {
-                AuthMockService().logout();
+                AuthService().logout();
               },
               child: Text('Logout'),
             ),
