18195a2d3a1c363ad4c1b7fa7e4a1c99dd0ae079
Saulo Gusmao <saulogusmaoribeiro@hotmail.com>
2022-12-26 18:39:00 -0300

    Chat Notification

---
+++ lib/core/services/notification/push_notification_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:proj_chat/core/models/chat_notification.dart';
+
+class PushNotificationService with ChangeNotifier {
+  List<ChatNotification> _items = [];
+
+  List<ChatNotification> get items {
+    return [..._items];
+  }
+
+  void add(ChatNotification notification) {
+    _items.add(notification);
+    notifyListeners();
+  }
+
+  void remove(int i) {
+    _items.removeAt(i);
+    notifyListeners();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:proj_chat/core/services/notification/push_notification_service.dart';
 import 'package:proj_chat/pages/auth_or_app_page.dart';
 import 'package:proj_chat/pages/auth_page.dart';
 import 'package:proj_chat/pages/loading_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => PushNotificationService(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: AuthOrAppPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: AuthOrAppPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
