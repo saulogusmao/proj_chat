66680ff02f1ab69f664207108d8353c944b94073
Saulo Gusmao <saulogusmaoribeiro@hotmail.com>
2023-01-04 00:53:27 -0300

    Initial firebase

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -7,23 +7,30 @@ import 'package:proj_chat/core/services/auth/auth_service.dart';
 import 'package:proj_chat/pages/auth_page.dart';
 import 'package:proj_chat/pages/chat_page.dart';
 import 'package:proj_chat/pages/loading_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<ChatUser?>(
-        stream: AuthService().userChanges,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return LoadingPage();
-          } else {
-            return snapshot.hasData ? ChatPage() : AuthPage();
-          }
-        },
-      ),
-    );
+    return FutureBuilder(
+        future: init(context),
+        builder: ((ctx, snapshot) {
+          return StreamBuilder<ChatUser?>(
+            stream: AuthService().userChanges,
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return LoadingPage();
+              } else {
+                return snapshot.hasData ? ChatPage() : AuthPage();
+              }
+            },
+          );
+        }));
   }
 }
